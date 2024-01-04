1f284f86449de5c1154dfe46b99ba63221b8cb7d
Cesare Sabattini <cesare.sabattini@studio.unibo.it>
2024-01-04 23:13:32 +0100

    ....

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
 app.use(morgan('common'));
 app.use(bodyParser.json({limit:'30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}));
-app.use(cors());
+
 
 app.use('/user',userRoutes);
 app.use('/post', postRoutes);
