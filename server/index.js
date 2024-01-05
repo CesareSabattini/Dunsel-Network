9381f13dafc5193e664f1048dd8b7b4564460282
Cesare Sabattini <cesare.sabattini@studio.unibo.it>
2024-01-05 11:26:53 +0100

    Updated vercel.json

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app= express();
 
 app.use(cors({
     origin:["https://dunsel-network.vercel.app"],
-    methods:["POST", "GET"],
+    methods:["POST", "GET", "DELETE"],
     credentials: true
 }))
 app.get('/', (req,res)=>{
