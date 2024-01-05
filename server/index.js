936aa9b831b5691c9050f1f56fd5066f9cd66b7a
Cesare Sabattini <cesare.sabattini@studio.unibo.it>
2024-01-05 13:07:37 +0100

    try

--- server/index.js
+++ server/index.js
@@ -17,12 +17,8 @@ app.use(cors(
     origin:["https://dunsel-network-front.vercel.app"],
     methods:["POST", "GET", "DELETE"],
     credentials: true,
-    allowedHeaders:[
-        { "key": "Access-Control-Allow-Credentials", "value": "true" },
-        { "key": "Access-Control-Allow-Origin", "value": "*" },
-        { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-    ]
+    allowedHeaders:['Access-Control-Allow-Origin' ]
+      
 }
 ))
 app.get('/', (req,res)=>{
